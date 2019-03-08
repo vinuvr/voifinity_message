d6ab09747afa16bfd78759e8ff25fd93e42412b8
“vinuvr” <vr.vinu849@gmail.com>
2019-03-08 17:52:07 +0530

    timesort key has been added into the voifinity_message_utils

--- src/voifinity_message_utils.erl
+++ src/voifinity_message_utils.erl
@@ -110,26 +110,34 @@ left_notification(MessageId,[H|T],Message) ->
   left_notification(MessageId,T,Message).
 server_status(From,MessageId,Message) ->
   NewMessageId = list_to_binary(binary_to_list(MessageId) ++ binary_to_list(<<"-sent">>)),
+  TimeSort = erlang:system_time(milli_seconds),
   Message1 = lists:keyreplace(<<"message_id">>,1,Message,{<<"message_id">>,NewMessageId}),
   Message2 = lists:keyreplace(<<"message_type">>,1,Message1,{<<"message_type">>,<<"server_status">>}),
-  Message3 = Message2 ++ [{<<"status">>,<<"sent">>},{<<"class">>,<<"messageStatus">>}],
-  FinalMessage = {[{<<"data">>,Message3}]},
+  Message3 = lists:keyreplace(<<"timesort">>,1,Message2,{<<"timesort">>,TimeSort}),
+  Message4 = Message3 ++ [{<<"status">>,<<"sent">>}
+                          ,{<<"class">>,<<"messageStatus">>}
+                         ],
+  FinalMessage = {[{<<"data">>,Message4}]},
   EncodedFinalMsg = jsx:encode(element(1,FinalMessage)),
   Publish        =emqx_message:make(From,EncodedFinalMsg),
   emqx:publish(Publish).
 server_status_delivered(From,MessageId,Message) ->
   NewMessageId = list_to_binary(binary_to_list(MessageId) ++ binary_to_list(<<"-delivered">>)),
+  TimeSort = erlang:system_time(milli_seconds),
   {{Year,Month,Day},{Hour,Minute,Second}} =calendar:now_to_universal_time(erlang:timestamp()),
-  Time=io_lib:format("~4.10.0B-~2.10.0B-~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B",[Year,Month, Day, Hour, Minute, Second]),
+  Time = io_lib:format("~4.10.0B-~2.10.0B-~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B",[Year,Month, Day, Hour, Minute, Second]),
   Message1 = lists:keyreplace(<<"message_id">>,1,Message,{<<"message_id">>,NewMessageId}),
   Message2 = lists:keyreplace(<<"message_type">>,1,Message1,{<<"message_type">>,<<"server_delivered_status">>}),
   Message3 = lists:keyreplace(<<"datetime">>,1,Message2,{<<"datetime">>,list_to_binary(Time)}),
-  Message4 = lists:keyreplace(<<"clientId">>,1,Message3,{<<"clientId">>,From}),
-  Message5 = Message4 ++ [{<<"status">>,<<"delivered">>},{<<"class">>,<<"messageStatus">>}],
+  Message4 = lists:keyreplace(<<"timesort">>,1,Message3,{<<"timesort">>,TimeSort}),
+  Message5 = Message4 ++ [{<<"status">>,<<"delivered">>}
+                          ,{<<"class">>,<<"messageStatus">>}
+                         ],
   FinalMessage = {[{<<"data">>,Message5}]},
   EncodedFinalMsg = jsx:encode(element(1,FinalMessage)),
   Publish = emqx_message:make(From,EncodedFinalMsg),
   emqx:publish(Publish).
+
 gregorian_days(A) ->
   A1=binary_to_list(A),
   {Y,M,D} = { list_to_integer(lists:sublist(binary_to_list(A),1,4))
